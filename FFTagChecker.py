c1522f5226b0bd44f838207601b5704c86bd6538
H0rusDoesSecurity <82106959+H0rusDoesSecurity@users.noreply.github.com>
2021-06-11 17:25:28 +0200

    Add files via upload

---
+++ FFTagChecker.py
@@ -0,0 +1,34 @@
+inp = open("inp.txt", "r")
+outp = open("outp.txt", "w")
+style = 0
+align = 0
+zeile = 1
+for line in inp:
+    a = line
+    style = a.count("!!i")+ a.count("!!b") + a.count("!!u") + a.count("!!l") - a.count("/!!")
+    if (style > 0):
+        print (style, "fehlende(r) [/style] Tag in Abschnit ", zeile, ": ", a[0:35], "...\n")
+    if (style <0):
+        print (style*-1, " [/style] Tag(s) zu viel in Abschnit ", zeile, ": ", a[0:35], "...\n")
+    align = a.count("??l")+ a.count("??r") + a.count("??c") - a.count("/??")
+    if (align > 0):
+        print (align, " fehlende(r) [/align] Tag in Abschnit ", zeile, ": ", a[0:35], "...\n")
+    if (align < 0):
+        print (align*-1, " [/align] Tag(s) zu viel in Abschnit ", zeile, ": ", a[0:35], "...\n")
+    a = a.replace("!!i", "[style type=\"italic\"]")
+    a = a.replace("!!b", "[style type=\"bold\"]")
+    a = a.replace("!!u", "[style type=\"underlined\"]")
+    a = a.replace("!!l", "[style type=\"linethrough\"]")
+    a = a.replace("/!!", "[/style]")
+    a = a.replace("??l", "[align type=\"left\"]")
+    a = a.replace("??r", "[align type=\"right\"]")
+    a = a.replace("??c", "[align type=\"center\"]")
+    a = a.replace("/??", "[/align]")
+    outp.write(a)
+    style = 0
+    align = 0
+    zeile = zeile + 1
+inp.close()
+outp.close()
+print ("done")
+input()
