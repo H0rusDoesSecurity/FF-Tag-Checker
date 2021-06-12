6fead5857041c5a355c226978bdb48fc63e9e2fc
H0rusDoesSecurity <82106959+H0rusDoesSecurity@users.noreply.github.com>
2021-06-12 08:55:45 +0200

    Update FFTagChecker.py

--- FFTagChecker.py
+++ FFTagChecker.py
@@ -7,14 +7,14 @@ for line in inp:
     a = line
     style = a.count("!!i")+ a.count("!!b") + a.count("!!u") + a.count("!!l") - a.count("/!!")
     if (style > 0):
-        print (style, "fehlende(r) [/style] Tag in Abschnit ", zeile, ": ", a[0:35], "...\n")
+        print (style, " fehlende(r) [/style] Tag in Zeile ", zeile, ": ", a[0:35].strip('\n'), "...\n",sep='')
     if (style <0):
-        print (style*-1, " [/style] Tag(s) zu viel in Abschnit ", zeile, ": ", a[0:35], "...\n")
+        print (style*-1, " [/style] Tag(s) zu viel in Zeile ", zeile, ": ", a[0:35].strip('\n'), "...\n", sep='')
     align = a.count("??l")+ a.count("??r") + a.count("??c") - a.count("/??")
     if (align > 0):
-        print (align, " fehlende(r) [/align] Tag in Abschnit ", zeile, ": ", a[0:35], "...\n")
+        print (align, " fehlende(r) [/align] Tag in Zeile ", zeile, ": ", a[0:35].strip('\n'), "...\n", sep='')
     if (align < 0):
-        print (align*-1, " [/align] Tag(s) zu viel in Abschnit ", zeile, ": ", a[0:35], "...\n")
+        print (align*-1, " [/align] Tag(s) zu viel in Zeile ", zeile, ": ", a[0:35].strip('\n'), "...\n", sep='')
     a = a.replace("!!i", "[style type=\"italic\"]")
     a = a.replace("!!b", "[style type=\"bold\"]")
     a = a.replace("!!u", "[style type=\"underlined\"]")
